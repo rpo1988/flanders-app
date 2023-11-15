ec65343751233e47b5a3bae14e6cc63b3b5f7c57
Rodrigo Ortega <rpo1988@gmail.com>
2023-11-15 18:00:56 +1000

    chore: add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./src/app/**/*.{ts,html}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#95bce2',
+      },
+    },
+  },
+  plugins: [],
+}
+
